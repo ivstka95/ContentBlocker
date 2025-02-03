cf707cb48c5028d4afa5c561307925756afb4051
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2025-02-03 22:35:21 +0400

    fixed build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ivan.karpiuk.adultblocker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ivan.karpiuk.adultblocker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
